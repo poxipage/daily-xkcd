d0decfd52c0c9c057f54bdd60fa44972082ac6fc
Øyvind Jalland schjerven <oyvind.jalland@hotmail.no>
2022-10-12 13:50:14 +0200

    init

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  let comic = ""
+  onMount(async () => {
+    await fetch("https://web-production-32e6.up.railway.app/https://xkcd.com/info.0.json")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      comic = data.img
+    })
+  })
+  
+</script>
+
+<img class="w-full h-full" src={comic} alt="daily comic">
+
+<style>
+</style>
